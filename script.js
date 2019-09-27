fb78eeb9f3b6f291f804514c4a08d39ed6c49027
Jason Osako <jasonosako@dhcp-v104-165.mobile.uci.edu>
2019-09-27 11:04:27 -0700

    additions to js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function generate() {
 
-    let 
+    let security = document.getElementById("dropdown").nodeValue;
+
+    let values ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[]\^_`{\}~";
+
+    let password = "";
+
+    for(var i = 0; i <=security; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+
+    document.getElementById("display").value = password;
+    
 }
